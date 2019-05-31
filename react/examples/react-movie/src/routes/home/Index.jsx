45cb18ea9141a252c83127dfc0ca7432ff5daaf7
ashton <codewin@126.com>
2019-06-01 07:47:05 +0800

    add react project

---
+++ react/examples/react-movie/src/routes/home/Index.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './Index.css';
+
+// 首页
+class Home extends Component {
+
+  render() {
+    return (
+      <div className="home">
+        DAVINCI
+      </div>
+    );
+  }
+}
+
+// Home.propTypes = {};
+export default Home;
